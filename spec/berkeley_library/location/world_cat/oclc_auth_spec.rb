c0822bb342fc63818984043a549321e805395d6b
Steve Sullivan <ssully28@gmail.com>
2025-01-10 15:16:51 -0800

    Fix bad expired token test!

--- spec/berkeley_library/location/world_cat/oclc_auth_spec.rb
+++ spec/berkeley_library/location/world_cat/oclc_auth_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'time'
 
 module BerkeleyLibrary
   module Location
@@ -12,17 +13,26 @@ module BerkeleyLibrary
           end
         end
 
-        it 'refreshes a token' do
+        it 'refreshes an expired token' do
           VCR.use_cassette('oclc_auth/refresh_token') do
+            # First get a token....
             token = OCLCAuth.instance.token
 
             # Need to set the token expiration to a time in the past
-            token[:expires_at] = (Time.now - 1).to_s
+            token[:expires_at] = (Time.now - 60).to_s
+            token[:access_token] = 'expired_token'
+
+            # Now we need to set the token instance to the token with the updated expiration
             OCLCAuth.instance.token = token
 
+            # Trigger a refresh by calling access_token
             OCLCAuth.instance.access_token
 
-            expect(OCLCAuth.instance.token[:access_token]).to be_a(String)
+            # Now check that the token has been refreshed
+            token = OCLCAuth.instance.token
+
+            expect(token[:access_token]).not_to eq('expired_token')
+            expect(Time.parse(token[:expires_at])).to be >= Time.now
           end
         end
 
